f6d407efffa99ba9068e057f6de24e0dac62f4d2
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2023-08-20 17:04:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search in a row-column sorted Matrix/search-in-a-rowcolumn-sorted-matrix.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to search a given number in row-column sorted matrix.
+    bool search(vector<vector<int> > mat, int n, int m, int x) 
+    {
+        // code here 
+       int j=m-1;
+       int i=0;
+       while(j<m&&i<n){
+           if(mat[i][j]==x) return 1;
+           else if(mat[i][j]>x) j--;
+           else i++;
+       }
+       return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+    cin>>t;
+    while(t--)
+    {
+
+ 		int n, m;
+    	cin>> n>> m;
+        vector<vector<int> > matrix(n);
+            
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(m, 0);
+            for( int j=0; j<m; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        int x;
+        cin>>x;
+        Solution obj;
+        if (obj.search(matrix,n,m,x)) 
+            cout << "1\n"; 
+        else
+            cout << "0\n"; 
+    }
+	return 0; 
+} 
+
+
+// } Driver Code Ends
