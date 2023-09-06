ff924efc829f4d66f25387324f73ef003f7fd2b3
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2023-09-07 00:14:02 +0530

    Updated Solution - GfG to GitHub

--- Easy/Wave Array/wave-array.cpp
+++ Easy/Wave Array/wave-array.cpp
@@ -13,16 +13,16 @@ class Solution{
     void convertToWave(int n, vector<int>& arr){
         
         // Your code here
-        if(n%2==0){
+        // if(n%2==0){
             for(int i=0;i<n-1;i+=2){
                swap(arr[i],arr[i+1]);
             }
-        }
-        else{
-            for(int i=0;i<n-2;i+=2){
-               swap(arr[i],arr[i+1]);
-            }
-        }
+        // }
+        // else{
+        //     for(int i=0;i<n-2;i+=2){
+        //       swap(arr[i],arr[i+1]);
+        //     }
+        // }
     }
 };
 
