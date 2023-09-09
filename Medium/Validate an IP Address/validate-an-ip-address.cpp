33733b4c926061d8c3698075af2457363f997cc6
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2023-09-10 01:02:49 +0530

    Updated Solution - GfG to GitHub

--- Medium/Validate an IP Address/validate-an-ip-address.cpp
+++ Medium/Validate an IP Address/validate-an-ip-address.cpp
@@ -11,63 +11,34 @@ class Solution {
     public:
         int isValid(string s) {
             // code here
-            if(s.length() > 15 or s.length() < 7) return 0;
-    
-    int dot = 0, res;
-    string str;
-    for(int i=0;i<s.length();i++)
-    {
-        if(s[i] == '.')
-        {
-            if(str.length() > 1 and str[0] == '0') return 0;
-            str.clear();
-            dot++;
-            if(dot > 3) return 0;
-        }
-        else if(s[i]>=48 and s[i]<=57)
-        {
-            str.push_back(s[i]);
-            if(str.length() == 4) return 0;
-            res = stoi(str);
-            if(res > 255) return 0;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    if(str.length() > 1 and str[0] == '0') return 0;
-    return 1;
+            s.push_back('.');
+            vector<string>v;
+            string ans="";
+            int num=0;
+            for(int i=0;i<s.size();i++){
+                if(s[i]=='.') {
+                    if(ans.size()==0) return false;
+                    v.push_back(ans);
+                    ans="";
+                    num++;
+                }
+                else {
+                    ans.push_back(s[i]);
+                }
+            }
             
-            
-            
-            
-            
-            
-            // vector<string>v;
-            // string ans="";
-            // int num=1;
-            // for(auto i:s){
-            //     if(i=='.') {
-            //         if(ans.size()==0) return false;
-            //         v.push_back(ans);
-            //         ans="";
-            //         num++;
-            //     }
-            //     else {
-            //         ans+=i;
-            //     }
-            // }
-            // if(num!=4) return false;
-            // for(auto i:v){
-            //     // cout<<i<<" ";
-            //     int a=stoi(i);
-            //     if(i.size()>1&&a==0) return false;
+            if(num!=4) return false;
+            for(auto i:v){
+                for (char character : i) {
+                    if (isalpha(character)) return false;
+                }
+                int a=stoi(i);
+                if(i.size()>1&&a<=9) return false;
                 
-            //     if(a>=0&&a<=255) continue;
-            //     else return 0;
-            // }
-            // return 1;
+                if(a>=0&&a<=255) continue;
+                else return 0;
+            }
+            return 1;
         }
 };
 
