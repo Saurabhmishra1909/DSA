9b01fb52007f4fb3496d2822fc12dd4479040c67
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2023-09-23 23:47:09 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Repeated Character/repeated-character.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        //code here.
+        unordered_map<int,int>m;
+        for(auto i:s){
+            m[i]++;
+        }
+        for(auto i:s){
+            if(m[i]>1) return i;
+        }
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
