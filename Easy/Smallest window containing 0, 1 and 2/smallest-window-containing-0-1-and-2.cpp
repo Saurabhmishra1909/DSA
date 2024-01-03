2b07a165a873571a4b868f2ab30867fd34ce0d75
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2024-01-03 21:48:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+ private:
+    int findMinLength(int i,int minLength,int zero,int one, int two) {
+        return min(minLength, (i - min(min(zero,one),two) + 1) );
+    }
+    
+  public:
+    int smallestSubstring(string S) {
+        // Code here
+        int zero = -1;
+        int one = -1;
+        int two = -1;
+        int minLength = INT_MAX;
+        
+        for(int i = 0; i < S.size(); i++) {
+            if(S[i] == '0')
+                zero = i;
+            else if(S[i] == '1')
+                one = i;
+            else
+                two = i;
+            
+            if(zero != -1 && one != -1 && two != -1)
+                minLength = findMinLength(i, minLength, zero, one, two);
+        }
+        
+        return (minLength != INT_MAX) ? minLength : -1;
+            
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.smallestSubstring(S);
+        cout << endl;
+    }
+}
+// } Driver Code Ends
